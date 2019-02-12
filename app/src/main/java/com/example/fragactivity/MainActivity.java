a570d8604abe8b73e65a573ae1e5749041528196
shame <shameeed97.pno@gmailcom>
2019-02-12 21:53:35 +0530

    send data from fragment to activity

--- app/src/main/java/com/example/fragactivity/MainActivity.java
+++ app/src/main/java/com/example/fragactivity/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.fragactivity;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements MessageFragment.MessageReadListener {
@@ -14,19 +14,19 @@ public class MainActivity extends AppCompatActivity implements MessageFragment.M
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragmentManager=getSupportFragmentManager();
-        if (findViewById(R.id.fragment_con)!=null){
-            if (savedInstanceState!=null){
+        fragmentManager = getSupportFragmentManager();
+        if (findViewById(R.id.fragment_con) != null) {
+            if (savedInstanceState != null) {
                 return;
             }
-            FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction().add(R.id.fragment_con,new MessageFragment(),null);
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().add(R.id.fragment_con, new MessageFragment(), null);
             fragmentTransaction.commit();
         }
     }
 
     @Override
     public void ReadListenerMessage(String message) {
-        TextView textView=findViewById(R.id.set_text);
+        TextView textView = findViewById(R.id.set_text);
         textView.setText(message);
 
     }
